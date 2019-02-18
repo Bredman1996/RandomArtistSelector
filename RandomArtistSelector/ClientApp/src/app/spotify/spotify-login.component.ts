de30660eb04dfe04abfb149ebca5f05627629ec3
Brandon <bredman@1800contacts.com>
2019-02-18 13:12:04 -0700

    redirecting to different page on successful login

--- RandomArtistSelector/ClientApp/src/app/spotify/spotify-login.component.ts
+++ RandomArtistSelector/ClientApp/src/app/spotify/spotify-login.component.ts
@@ -4,7 +4,6 @@ import { Component, Inject } from '@angular/core';
 import { SpotifyService } from '../services/spotify-controller';
 import { ActivatedRoute } from '@angular/router';
 import { AccessTokenRequest } from '../Models/AccessTokenRequest';
-//import * as math from 'mathjs';
 
 
 @Component({
@@ -19,14 +18,16 @@ export class SpotifyLoginComponent {
   constructor(private spotifyService: SpotifyService, private route: ActivatedRoute, @Inject('BASE_URL') baseUrl: string) {
     if (sessionStorage.accessToken) {
       window.location.assign(baseUrl + "playlist")
-    } 
-    this.route.queryParams.subscribe(params => {
-      this.request.code = params['code'];
+    }
+      this.route.queryParams.subscribe(params => {
+        this.request.code = params['code'];
     });
-    spotifyService.getAccessToken(baseUrl + "api/spotify/GetUserInfo", this.request).subscribe(result => {
-      sessionStorage.setItem('url', result["href"]);
-      sessionStorage.setItem('accessToken', result["authToken"]);
-      window.location.assign(baseUrl + "playlist");
-    });
+    if (this.request.code) {
+      spotifyService.getAccessToken(baseUrl + "api/spotify/GetUserInfo", this.request).subscribe(result => {
+        sessionStorage.setItem('url', result["href"]);
+        sessionStorage.setItem('accessToken', result["authToken"]);
+        window.location.assign(baseUrl + "playlist");
+      });
+    }
   }
 }
